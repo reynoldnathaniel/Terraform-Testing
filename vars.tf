5433220485dbf805eaca73188410e6be950a08db
reynoldnathaniel <reynoldnathaniel@outlook.com>
2020-09-15 21:57:26 +0800

    Successfully created an instance Ubuntu and executed a bash script

---
+++ vars.tf
@@ -0,0 +1,28 @@
+variable "AWS_ACCESS_KEY" {
+    type = string
+}
+variable "AWS_SECRET_KEY" {
+    type = string
+}
+variable "AWS_REGION"{
+    type = string
+}
+
+variable "AMIS"{
+    type = map(string)
+    default = {
+        ap-southeast-1 = "ami-0ffa7deb237ae4ad2"
+    }
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+    default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+    default = "mykey.pub"
+}
+
+variable "INSTANCE_USERNAME" {
+    default = "ubuntu"
+}
