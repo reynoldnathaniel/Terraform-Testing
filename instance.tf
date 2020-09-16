a64822d04362f3e2804a260b945f29c73b4d0f79
reynoldnathaniel <reynoldnathaniel@outlook.com>
2020-09-16 21:21:01 +0800

    Deployed EC2 instance in public subnet and RDS in private subnet. EC2 instance can connect to RDS

--- instance.tf
+++ instance.tf
@@ -6,23 +6,9 @@ resource "aws_instance" "example" {
   subnet_id = aws_subnet.main-public-1.id
 
   # the security group
-  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+  vpc_security_group_ids = [aws_security_group.example-instance.id]
 
   # the public SSH key
   key_name = aws_key_pair.mykeypair.key_name
-}
 
-resource "aws_ebs_volume" "ebs-volume-1" {
-  availability_zone = "ap-southeast-1a"
-  size              = 20
-  type              = "gp2"
-  tags = {
-    Name = "extra volume data"
-  }
-}
-
-resource "aws_volume_attachment" "ebs-volume-1-attachment" {
-  device_name = "/dev/xvdh"
-  volume_id   = aws_ebs_volume.ebs-volume-1.id
-  instance_id = aws_instance.example.id
 }
