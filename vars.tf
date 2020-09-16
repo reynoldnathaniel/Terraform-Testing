5aa79900b8e29efc4c1516a8694b908d5ccdc8fd
reynoldnathaniel <reynoldnathaniel@outlook.com>
2020-09-16 14:36:03 +0800

    Successfully outputted the attributes, IP Address, of the EC2 instance

--- vars.tf
+++ vars.tf
@@ -8,17 +8,17 @@ variable "AWS_REGION"{
     type = string
 }
 
+variable "AMIS"{
+    type = map(string)
+    default = {
+        ap-southeast-1 = "ami-0ffa7deb237ae4ad2"
+    }
+}
+
 variable "PATH_TO_PRIVATE_KEY" {
     default = "mykey"
 }
 
 variable "PATH_TO_PUBLIC_KEY" {
     default = "mykey.pub"
-}
-
-variable "INSTANCE_USERNAME" {
-  default = "Terraform"
-}
-
-variable "INSTANCE_PASSWORD" {
 }
