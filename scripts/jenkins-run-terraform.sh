504987f04f11224c14b577096ea6f3674dc6afdb
Reynold Nathaniel <reynoldnathaniel@outlook.com>
2020-09-17 14:56:58 +0800

    Reconfigure the terraform init and directory issue

--- scripts/jenkins-run-terraform.sh
+++ scripts/jenkins-run-terraform.sh
@@ -1,13 +1,12 @@
+
 #!/bin/bash
 set -ex
 AWS_REGION="ap-southeast-1"
-cd jenkins-packer-demo
-S3_BUCKET=`aws s3 ls --region $AWS_REGION |grep terraform-state |tail -n1 |cut -d ' ' -f3`
 pwd
 ls
+S3_BUCKET=`aws s3 ls --region $AWS_REGION |grep terraform-state |tail -n1 |cut -d ' ' -f3`
 sed -i 's/terraform-state-xx70dpnh/'${S3_BUCKET}'/' backend.tf
 sed -i 's/#//g' backend.tf
 aws s3 cp s3://${S3_BUCKET}/amivar.tf amivar.tf --region $AWS_REGION
-terraform init
-terraform apply -auto-approve -var APP_INSTANCE_COUNT=1 -target aws_instance.app-instance
-
+terraform init -reconfigure
+terraform apply -auto-approve -var APP_INSTANCE_COUNT=1 -var AWS_REGION="ap-southeast-1" -target aws_instance.app-instance
