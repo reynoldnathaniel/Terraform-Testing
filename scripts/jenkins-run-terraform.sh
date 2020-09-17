4841b7bb10c0e700cb9b4da0468d6cd61651867e
Reynold Nathaniel <reynoldnathaniel@outlook.com>
2020-09-17 14:34:58 +0800

    Update jenkins-run-terraform.sh

--- scripts/jenkins-run-terraform.sh
+++ scripts/jenkins-run-terraform.sh
@@ -3,6 +3,8 @@ set -ex
 AWS_REGION="ap-southeast-1"
 cd jenkins-packer-demo
 S3_BUCKET=`aws s3 ls --region $AWS_REGION |grep terraform-state |tail -n1 |cut -d ' ' -f3`
+pwd
+ls
 sed -i 's/terraform-state-xx70dpnh/'${S3_BUCKET}'/' backend.tf
 sed -i 's/#//g' backend.tf
 aws s3 cp s3://${S3_BUCKET}/amivar.tf amivar.tf --region $AWS_REGION
